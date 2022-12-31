f86bc2b05949d24caed43f846cb268d13dd4833c
Hazx <hazx632823367@gmail.com>
2022-12-31 15:25:04 +0800

    更新1.0

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+stcnv_img=hmengine-stcnv
+stcnv_ver=1.0
+
+## 清理工作目录
+if [ -e build_${stcnv_img} ];then
+    rm -fr build_${stcnv_img}
+fi
+if [ -e output/${stcnv_img}-${stcnv_ver}.tar ];then
+    rm -fr output/${stcnv_img}-${stcnv_ver}.tar
+fi
+
+mkdir -p build_${stcnv_img}
+mkdir -p output
+
+## 构建
+cp -R build build_${stcnv_img}/
+cat <<EOF > build_${stcnv_img}/build/Dockerfile
+FROM ubuntu:20.04
+LABEL maintainer="hazx632823367@gmail.com"
+LABEL Version="${stcnv_ver}"
+COPY * /root/stcnv-build/
+COPY nginx-conf /root/stcnv-build/nginx-conf
+COPY api /root/stcnv-build/api
+RUN bash /root/stcnv-build/IDR-build-sh
+CMD /web_server/webserver.sh
+EOF
+
+## 编译与导出
+docker build -t ${stcnv_img}:${stcnv_ver} build_${stcnv_img}/build/
+docker save -o output/${stcnv_img}-${stcnv_ver}.tar ${stcnv_img}:${stcnv_ver}
+
+## 清理垃圾
+docker rmi ${stcnv_img}:${stcnv_ver}
+rm -fr build_${stcnv_img}
+
+echo "Docker build finished."
+echo "Image name: ${stcnv_img}:${stcnv_ver}"
+echo "Image Path: output/${stcnv_img}-${stcnv_ver}.tar"
---
+++ build/api/index.php
@@ -0,0 +1,112 @@
+<?php
+
+// 请求方式: POST
+// 请求路径: /convert 或 /?convert
+// 请求参数: text: 要转换的文本
+// 　　　　  mode: 转换模式 s2ttw/ttw2s
+
+// 返回结果: JSON
+// 　　　　  code: 执行结果码
+// 　　　　  data: 主体数据（包含mode、text）
+// 　　　　        mode: 当前执行的转换模式
+// 　　　　        text: 转换后的文本
+// 　　　　  msg: 错误提示信息
+// 　　　　  ver: 当前程序版本
+// 　　　　  execTime: 程序执行时长
+
+// 结果码: 0 正常结束
+// 　　　  1 缺少必要参数
+// 　　　  2 mode 参数错误
+
+// 配置
+$stcnv_ver = "1.0";
+
+// 记录程序开始时间
+$execTimeStart = microtime(true);
+
+// 判断传参
+if(!isset($_GET['convert'])){
+    echo "ok.";
+    die;
+}
+if(!isset($_POST['text'])){
+    returnErrorJson("缺少参数 text", 1);
+    die;
+}else if(!isset($_POST['mode'])){
+    returnErrorJson("缺少参数 mode", 1);
+    die;
+}else if($_POST['mode'] != "s2ttw" && $_POST['mode'] != "ttw2s"){
+    returnErrorJson("mode 参数错误", 2);
+    die;
+}
+
+// 执行转换
+$text_orig = $_POST['text'];
+$cnv_mode = addslashes($_POST['mode']);
+$text_cnv = stcnv($text_orig, $cnv_mode);
+
+// 返回结果
+returnCnvJson($text_cnv, $cnv_mode);
+die;
+
+
+
+// 返回执行结果  传参: 错误提示 执行结果码
+function returnCnvJson($text, $mode){
+    global $execTimeStart;
+    global $stcnv_ver;
+    $execTimeEnd = microtime(true);
+    $execTime = number_format($execTimeEnd - $execTimeStart, 2);
+    $json_array = array(
+        "code" => 0,
+        "data" => array(
+            "mode" => $mode,
+            "text" => $text
+        ),
+        "msg" => "",
+        "ver" => $stcnv_ver,
+        "execTime" => $execTime
+    );
+    $json_data = json_encode($json_array, JSON_UNESCAPED_UNICODE);
+    header('Content-Type: application/json');
+    echo $json_data;
+
+    return;
+}
+
+// 返回错误信息  传参: 错误提示 执行结果码
+function returnErrorJson($msg, $code){
+    global $execTimeStart;
+    global $stcnv_ver;
+    $execTimeEnd = microtime(true);
+    $execTime = number_format($execTimeEnd - $execTimeStart, 2);
+    $json_array = array(
+        "code" => $code,
+        "data" => array(),
+        "msg" => $msg,
+        "ver" => $stcnv_ver,
+        "execTime" => $execTime
+    );
+    $json_data = json_encode($json_array, JSON_UNESCAPED_UNICODE);
+    header('Content-Type: application/json');
+    echo $json_data;
+
+    return;
+}
+
+function stcnv($text_orig, $cnv_type){
+    if($cnv_type == "s2ttw"){
+        $stcnv_fun = opencc_open("s2twp.json");
+    }else if($cnv_type == "ttw2s"){
+        $stcnv_fun = opencc_open("tw2sp.json");
+    }
+    $text_cnv = opencc_convert($text_orig, $stcnv_fun);
+    opencc_close($stcnv_fun);
+
+    return $text_cnv;
+}
+
+
+
+
+
